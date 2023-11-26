c9b0604248df577566fec2b6594d6ee668e42a2d
KitasanYuu <tamamoratizel@gmail.com>
2023-11-27 00:51:25 +0800

    临时增加了射击显示点，增加了测试用材质，修改测试场景部分文件

--- Assets/Scripts/SelfMade/ShootController/TPSShootController.cs
+++ Assets/Scripts/SelfMade/ShootController/TPSShootController.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 using StarterAssets;
+using UnityEngine.InputSystem;
 
 public class TPSShootController : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
     [SerializeField] private float normalSensitivity;
     [SerializeField] private float aimSensitivity;
+    [SerializeField] private LayerMask aimColliderLayerMask = new LayerMask();
+    [SerializeField] private Transform debugTransform;
 
     private AvatarController avatarController;
     private StarterAssetsInputs starterAssetsInputs;
@@ -29,6 +32,13 @@ public class TPSShootController : MonoBehaviour
         {
             aimVirtualCamera.Priority = 5;
             avatarController.SetSensitivity(normalSensitivity);
+        }
+
+        Vector2 screenCenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if(Physics.Raycast(ray,out RaycastHit raycastHit, 999f, aimColliderLayerMask))
+        {
+            debugTransform.position = raycastHit.point;
         }
 
     }
