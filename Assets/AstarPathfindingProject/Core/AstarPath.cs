3540b9c4f423af4c5ef17639ba3c7047054def5e
KitasanYuu <tamamoratizel@gmail.com>
2023-12-24 22:42:16 +0800

    把AstarPath报错干掉
    注了两行Debug

--- Assets/AstarPathfindingProject/Core/AstarPath.cs
+++ Assets/AstarPathfindingProject/Core/AstarPath.cs
@@ -807,9 +807,9 @@ public class AstarPath : VersionedMonoBehaviour {
 			if (logPathResults == PathLog.InGame) {
 				inGameDebugPath = debug;
 			} else if (path.error) {
-				Debug.LogWarning(debug);
+				//Debug.LogWarning(debug);
 			} else {
-				Debug.Log(debug);
+				//Debug.Log(debug);
 			}
 		}
 	}
