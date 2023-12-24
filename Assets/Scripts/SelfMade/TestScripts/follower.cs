e6bc5c38935798fa3484d2b5f28bca06f428eeed
KitasanYuu <tamamoratizel@gmail.com>
2023-12-24 22:41:04 +0800

    大改Follow脚本，使用A*路径算法
    大改Follow脚本，使用A*路径算法

--- Assets/Scripts/SelfMade/TestScripts/follower.cs
+++ Assets/Scripts/SelfMade/TestScripts/follower.cs
@@ -1,6 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.AI;
+using Pathfinding;
+using System.Collections.Generic;
 using TMPro;
 #if UNITY_EDITOR
 using UnityEditor;
@@ -10,7 +12,7 @@ namespace Partner
 {
     public class Follower : MonoBehaviour
     {
-        public Transform targetToFollow;
+        public GameObject targetToFollow;
         public Vector3 sectorDirection = Vector3.forward;
         public float followSpeed = 2f;
         public float stoppingDistance = 0.5f;
@@ -28,11 +30,17 @@ namespace Partner
         private bool hasReachedTargetPoint = true; // 初始设为true，以允许首次目标点的设置
         public float rotationSpeed = 5.0f;
 
-        private NavMeshAgent navMeshAgent;
-        private NavMeshPath navMeshPath;
+        //以下是Astar算法所用的参数
+        private Seeker seeker;
+        public List<Vector3> aimPoint;
+
+        //这些是之前用NavMesh导航时所用的参数，现在使用Astar算法后弃用
+        //private NavMeshAgent navMeshAgent;
+        //private NavMeshPath navMeshPath;
 
         //这里是仅用作外部调取的函数
         public Vector3 MoveDirection;//用于外部获取角色方向
+        public Vector3 CTargetPosition;//用于外部获取目标点
 
         private float currentSpeed;
 
@@ -40,30 +48,89 @@ namespace Partner
 
         void Start()
         {
-            navMeshAgent = GetComponent<NavMeshAgent>();
-            navMeshPath = new NavMeshPath();
-            if (targetToFollow != null)
-            {
-                lastTargetPosition = targetToFollow.position;
-                isInsideSector = true;
-                UpdateTargetPoint();
-            }
-        }
-        private IEnumerator MoveAfterDelay(float delay)
-        {
-            yield return new WaitForSeconds(delay);
-            //MoveTowardsTarget();
-            navMeshAgent.SetDestination(targetToFollow.TransformPoint(currentTargetPoint));
-            Debug.Log(navMeshPath.status);
-            hasReachedTargetPoint = true;
+            GetComponent();
+            FollowTargetInit();
+
+            seeker.pathCallback += OnPathComplete; //在每次回调成功后把新路径点加在数组后面
+
         }
 
         void Update()
         {
+            CanISpwanTargetPoint();
+            SeekerCalcu();
+            MovingStart();
+
+        }
+
+        //这个方法用来启动Seeker的路径计算
+        private void SeekerCalcu()
+        {
+            Vector3 TargetPosition = targetToFollow.transform.TransformPoint(currentTargetPoint);
+            seeker.StartPath(transform.position, TargetPosition);
+        }
+
+        //一个方法，用来在回调后保存Path数组的值
+        void OnPathComplete(Path path)
+        {
+            aimPoint = new List<Vector3>(path.vectorPath);
+        }
+
+        //协程计时x秒后执行移动操作
+        private IEnumerator MoveAfterDelay(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+
+            //下面是移动方法的调用
+            AStarMoving();
+            //MoveTowardsTarget();
+            //navMeshAgent.SetDestination(targetToFollow.TransformPoint(currentTargetPoint));
+            //Debug.Log(navMeshPath.status);
+            //hasReachedTargetPoint = true;
+        }
+
+        //用于在Start方法中获取当前物体上的组件
+        private void GetComponent()
+        {
+            //navMeshAgent = GetComponent<NavMeshAgent>();
+            //navMeshPath = new NavMeshPath();
+
+            seeker = GetComponent<Seeker>();
+        }
+
+        //用于在Star方法中初始化随机跟随目标的生成
+        private void FollowTargetInit()
+        {
+            if (targetToFollow != null)
+            {
+                lastTargetPosition = targetToFollow.transform.position;
+                isInsideSector = true;
+                UpdateTargetPoint();
+            }
+        }
+
+        //这个是用来判定跟随者是否已经进入了会刷新随机目标点的自定义范围内
+        protected bool IsInSector(Vector3 position)
+        {
+            Vector3 worldSectorDirection = targetToFollow.transform.TransformDirection(sectorDirection.normalized);
+            Vector3 toPosition = position - targetToFollow.transform.position;
+
+            toPosition.y = 0; // 忽略 Y 轴的变化
+
+            float angleToPosition = Vector3.Angle(worldSectorDirection, toPosition);
+
+            //Debug.Log("Angle to Position: " + angleToPosition + ", Radius: " + toPosition.magnitude);
+
+            return angleToPosition <= angle / 2 && toPosition.magnitude <= radius;
+        }
+
+        //下面是用来判定是否应该执行生成随机目标点的判定逻辑
+        protected void CanISpwanTargetPoint()
+        {
             if (targetToFollow != null)
             {
                 //Debug.Log(isInsideSector);
-                bool hasMoved = Vector3.Distance(targetToFollow.position, lastTargetPosition) > 0.01f;
+                bool hasMoved = Vector3.Distance(targetToFollow.transform.position, lastTargetPosition) > 0.01f;
                 //Debug.Log("正在移动"+hasMoved);
                 if (hasMoved)
                 {
@@ -101,43 +168,23 @@ namespace Partner
                         isInsideSector = true;
                     }
                     canGeneratePoint = true;
-
                 }
-
-                lastTargetPosition = targetToFollow.position;
-                if (!isInsideSector && !IsInvoking("MoveAfterDelay"))
-                {
-                    StartCoroutine(MoveAfterDelay(DelayStartTime)); // x 是延迟时间，以秒为单位
-                }
-
+                lastTargetPosition = targetToFollow.transform.position;
             }
         }
 
-        bool IsInSector(Vector3 position)
+        //这是用来刷新目标点时调用的函数
+        protected void UpdateTargetPoint()
         {
-            Vector3 worldSectorDirection = targetToFollow.TransformDirection(sectorDirection.normalized);
-            Vector3 toPosition = position - targetToFollow.position;
-            toPosition.y = 0; // 忽略 Y 轴的变化
-
-            float angleToPosition = Vector3.Angle(worldSectorDirection, toPosition);
-
-            //Debug.Log("Angle to Position: " + angleToPosition + ", Radius: " + toPosition.magnitude);
-
-            return angleToPosition <= angle / 2 && toPosition.magnitude <= radius;
+            currentTargetPoint = GenerateRandomPointInSector(targetToFollow.transform.position, sectorDirection, radius, angle, minDistance); // 假设 x 是最小距离
+            currentTargetPoint = targetToFollow.transform.InverseTransformPoint(currentTargetPoint);
+            CTargetPosition = currentTargetPoint;//给外部读取的目标点值
         }
 
-
-
-
-        void UpdateTargetPoint()
+        //这是随机生成目标点的函数
+        protected Vector3 GenerateRandomPointInSector(Vector3 center, Vector3 direction, float radius, float angle, float minDistance)
         {
-            currentTargetPoint = GenerateRandomPointInSector(targetToFollow.position, sectorDirection, radius, angle, minDistance); // 假设 x 是最小距离
-            currentTargetPoint = targetToFollow.InverseTransformPoint(currentTargetPoint);
-        }
-
-        Vector3 GenerateRandomPointInSector(Vector3 center, Vector3 direction, float radius, float angle, float minDistance)
-        {
-            Vector3 worldSectorDirection = targetToFollow.TransformDirection(direction.normalized);
+            Vector3 worldSectorDirection = targetToFollow.transform.TransformDirection(direction.normalized);
             Vector3 randomPoint;
 
             do
@@ -151,11 +198,23 @@ namespace Partner
             return randomPoint;
         }
 
+        //用来判定启动协程计时执行Move的函数
+        private void MovingStart()
+        {
+            if (!isInsideSector && !IsInvoking("MoveAfterDelay"))
+            {
+                StartCoroutine(MoveAfterDelay(DelayStartTime)); // x 是延迟时间，以秒为单位
+            }
+        }
 
+        //正式的Move移动函数，只能和A*用一个，放弃维护了xx
         void MoveTowardsTarget()
         {
-            Vector3 worldTargetPoint = targetToFollow.TransformPoint(currentTargetPoint);
-            worldTargetPoint.y = transform.position.y; // 忽略 Y 轴的变化
+            Vector3 worldTargetPoint = targetToFollow.transform.TransformPoint(currentTargetPoint);
+
+            //角色的Container的Y轴本来就应该处在0，如果强行把Y轴变为0在用Rotation转向时会出错
+            //worldTargetPoint.y = transform.position.y; // 忽略 Y 轴的变化
+
             float distanceToTarget = Vector3.Distance(transform.position, worldTargetPoint);
             float currentSpeed;
 
@@ -191,43 +250,129 @@ namespace Partner
                     MoveDirection = moveDirection;
                 }
             }
-            else
-            {
-                isInsideSector = true;
-                hasReachedTargetPoint = true;
-                Debug.LogWarning("REACHED TARGET POINT");
-            }
 
         }
 
-        void OnDrawGizmos()
+        //使用A*寻路移动的功能
+        protected void AStarMoving()
         {
-            if (navMeshPath != null && navMeshPath.corners.Length > 1)
+            //这里用if是因为在抵达目标点一瞬间数组会只有一位数0，此时Vector3 aimPoint[1]会取不到值
+            int Count = aimPoint.Count;
+            Vector3 Destination;
+            //Debug.LogError(Count);
+
+            if(Count == 1)
             {
-                Gizmos.color = Color.blue;
-                for (int i = 0; i < navMeshPath.corners.Length - 1; i++)
+                Destination = aimPoint[index: 0];
+            }
+            else
+            {
+                Destination = aimPoint[index: 1];
+            }
+
+            float distanceToTarget = CalculateTotalLength(aimPoint);
+            float currentSpeed;
+
+            //Debug.LogWarning(distanceToTarget);
+
+            if (distanceToTarget > stoppingDistance)
+            {
+                // 计算当前速度，使其逐渐减小直到0
+                currentSpeed = followSpeed * (distanceToTarget / stoppingDistance);
+
+                if (distanceToTarget > accelerationDistance)
                 {
-                    Gizmos.DrawLine(navMeshPath.corners[i], navMeshPath.corners[i + 1]);
+                    // 如果距离小于加速的阈值距离，使用加速速度
+                    currentSpeed = accelerationSpeed;
+                }
+                else if (currentSpeed > followSpeed)
+                {
+                    currentSpeed = followSpeed;
+                }
+
+                if (aimPoint != null && aimPoint.Count != 0)
+                {
+                    // 计算移动方向
+                    Vector3 MoveDir = (aimPoint[1] - transform.position).normalized;
+                    //Debug.Log(currentSpeed);
+                    //移动角色
+                    transform.position = Vector3.MoveTowards(transform.position, Destination, currentSpeed * Time.deltaTime);
+
+                    // 使物体朝向移动方向
+                    if (MoveDir != Vector3.zero)
+                    {
+                        // 计算目标朝向
+                        Quaternion targetRotation = Quaternion.LookRotation(MoveDir);
+
+                        //Debug.LogError(targetRotation);
+                        //Debug.LogWarning(transform.rotation);
+
+                        // 使用Slerp插值来平滑地转向目标朝向
+                        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, 5 * Time.deltaTime);
+                    }
+
+                    //如果目标抵达了一个路径节点则将这个节点删去
+                    if (Vector3.Distance(aimPoint[1], transform.position) <= 1f)
+                    {
+                        aimPoint.RemoveAt(1);
+                    }
                 }
             }
+            else
+            {
+                //移动完成后跟随者是肯定在目标点刷新范围内的
+                isInsideSector = true;
+                hasReachedTargetPoint = true;
+                //Debug.LogWarning("REACHED TARGET POINT");
+            }
+        }
+
+        //用来计算A*路径上的总长度函数
+        float CalculateTotalLength(List<Vector3> points)
+        {
+            float totalLength = 0f;
+
+            // 遍历点列表，计算相邻点之间的距离并相加
+            for (int i = 0; i < points.Count - 1; i++)
+            {
+                totalLength += Vector3.Distance(points[i], points[i + 1]);
+            }
+
+            return totalLength;
+        }
+
+
+
+        //下面是Gizmos上的绘制，仅在编辑视角生效
+        void OnDrawGizmos()
+        {
+            //这个是使用NavMesh时所用的绘制代码
+            //if (navMeshPath != null && navMeshPath.corners.Length > 1)
+            //{
+            //    Gizmos.color = Color.blue;
+            //    for (int i = 0; i < navMeshPath.corners.Length - 1; i++)
+            //    {
+            //        Gizmos.DrawLine(navMeshPath.corners[i], navMeshPath.corners[i + 1]);
+            //    }
+            //}
 
             if (targetToFollow != null)
             {
-                Vector3 worldSectorDirection = targetToFollow.TransformDirection(sectorDirection.normalized);
+                Vector3 worldSectorDirection = targetToFollow.transform.TransformDirection(sectorDirection.normalized);
 
                 // 绘制扇形区域的填充
-                DrawSector(targetToFollow.position, worldSectorDirection, radius, angle);
+                DrawSector(targetToFollow.transform.position, worldSectorDirection, radius, angle);
 
                 // 可视化目标点
                 Gizmos.color = Color.green;
-                Gizmos.DrawWireSphere(targetToFollow.TransformPoint(currentTargetPoint), stoppingDistance);
+                Gizmos.DrawWireSphere(targetToFollow.transform.TransformPoint(currentTargetPoint), stoppingDistance);
 
                 // 绘制跟随者到目标点的轨迹
                 Gizmos.color = Color.white;
-                Gizmos.DrawLine(transform.position, targetToFollow.TransformPoint(currentTargetPoint));
+                Gizmos.DrawLine(transform.position, targetToFollow.transform.TransformPoint(currentTargetPoint));
 
                 // 绘制表示最小距离到最大距离范围的覆盖面
-                DrawSectorRange(targetToFollow.position, worldSectorDirection, minDistance, radius, angle);
+                DrawSectorRange(targetToFollow.transform.position, worldSectorDirection, minDistance, radius, angle);
             }
         }
 
