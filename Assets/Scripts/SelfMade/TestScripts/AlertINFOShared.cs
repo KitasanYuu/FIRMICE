795b2820426bd2307b582720362e94ec63803a14
KitasanYuu <tamamoratizel@gmail.com>
2024-01-20 00:55:12 +0800

    区域内信息接收者传递脚本单独分离

---
+++ Assets/Scripts/SelfMade/TestScripts/AlertINFOShared.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using TestField;
+using UnityEngine;
+
+[RequireComponent(typeof(AlertLogic))]
+public class AlertINFOShared : MonoBehaviour
+{
+    private List<GameObject> OtherReceiver = new List<GameObject>();
+
+    //private BroadCasterInfoContainer BCIC;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
+
