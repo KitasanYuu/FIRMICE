eb080ce181f9840f9c87edf63bc7976ef8ece122
KitasanYuu <tamamoratizel@gmail.com>
2024-01-22 00:12:53 +0800

    AI-Identity新增Cover类和Prototype

--- Assets/Scripts/SelfMade/TestScripts/Identity.cs
+++ Assets/Scripts/SelfMade/TestScripts/Identity.cs
@@ -7,22 +7,37 @@ namespace TestField
 {
     public class Identity : MonoBehaviour
     {
-        [SerializeField]
-        [FixedValues("Player", "Partner", "Enemy", "Neutral", "TrainingTarget")]
+        [SerializeField, FixedValues("Player", "Partner", "Enemy", "Neutral", "TrainingTarget","TestPrototype","Cover")]
         private string MasterIdentity;
+        [SerializeField, FixedValues("null","FullCover", "HalfCover"),ShowIf(nameof(CoverSerizes))]
+        private string CoverType;
+        public bool CoverSerizes() => MasterIdentity == "Cover";
 
         [HideInInspector]
         public string MasterID;
+        [HideInInspector]
+        public string Covertype;
         // Start is called before the first frame update
         void Start()
         {
             MasterID = MasterIdentity;
+            Covertype = CoverType;
         }
 
         // Update is called once per frame
         void Update()
         {
             MasterID = MasterIdentity;
+            Covertype = CoverType;
+            CoverTypeUpdate();
+        }
+
+        private void CoverTypeUpdate()
+        {
+            if (MasterIdentity != "Cover")
+            {
+                CoverType = null;
+            }
         }
     }
 }
