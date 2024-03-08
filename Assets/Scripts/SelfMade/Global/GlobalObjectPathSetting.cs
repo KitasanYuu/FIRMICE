e3f869fee9eb370d995e20e172f71540db835da8
KitasanYuu <tamamoratizel@gmail.com>
2024-03-08 23:29:43 +0800

    Data-新增SO外部存储数据

---
+++ Assets/Scripts/SelfMade/Global/GlobalObjectPathSetting.cs
@@ -0,0 +1,11 @@
+using CustomInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GlobalObjectPathSetting", menuName = "Global/GlobalObjectPathSetting", order = 1)]
+public class GlobalObjectPathSetting:ScriptableObject
+{
+    public FolderPath BulletEffect = new FolderPath();
+
+}
