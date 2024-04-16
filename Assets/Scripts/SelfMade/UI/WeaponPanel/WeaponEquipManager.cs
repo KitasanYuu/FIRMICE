43eab30fc8741ec8d06c1d108474df6ced7378d1
KitasanYuu <tamamoratizel@gmail.com>
2024-04-16 18:43:54 +0800

    暂存

---
+++ Assets/Scripts/SelfMade/UI/WeaponPanel/WeaponEquipManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponEquipManager : MonoBehaviour
+{
+    public List<CurrentSelectedWeapon> CSW = new List<CurrentSelectedWeapon>();
+
+}
